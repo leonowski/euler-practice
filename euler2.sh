9b40aa675ef5f05587f14f11f83bf20a933d0fa9
U-FARADAY\leonowski <leonowski@earthlink.net>
2018-02-24 21:52:08 -0800

    finished euler2

--- euler2.sh
+++ euler2.sh
@@ -1,22 +1,45 @@
 #!/usr/bin/bash
 
 #initialize array with first 2 fibonacci numbers
-array=(0 1)
+array=(1 2)
 
-x=0
+#actually, don't need to keep count
+#keep count
+#x=0
 
-while [[ $x < 10 ]];
+
+#while loop to put fibonacci sequence in array.  change upper range to get a limit.  this problem says the last number should be less than 4 million
+while [[ ${array[-1]} -lt 4000000 ]];
 do
-  array+=${array[-1]}+${array[-2]}
-  x=(${a[${#a[@]}+1]})
-  printf '%s\n' "${array[@]}"
+  nextnum=$(( ${array[-1]}+${array[-2]} ))
+  array+=("$nextnum")
+#comment this out since it does nothing.  while loop goes without counting  #x=$((x+1))
 done
 
-printf '%s\n' "${array[@]}"
+#uncomment to print array line by line
+#printf '%s\n' "${array[@]}"
 
-#for loop again to add up array elements
-#tot=0
-#for i in ${array[@]}; do
-#  let tot+=$i
-#done
-#echo "Total: $tot"
+#put even numbers in array in to new array
+newarray=()
+for i in "${array[@]}";
+do
+
+  if [[ $i%2 -eq 0 ]]; then
+    newarray+=("$i")
+  fi
+done
+
+#print new even array
+#printf '%s\n' "${newarray[@]}"
+
+#finally, add the elements of the new array.  they should all be even.
+
+total=0
+for z in "${newarray[@]}";
+do
+total=$((total + z))
+##keeping count here## printf '%s\n' "$total"
+done
+
+printf '%s\n' "$total"
+
