d95a666c70ff9a1f55b3bd40b1ed355d7ad974bd
U-FARADAY\leonowski <leonowski@earthlink.net>
2018-02-27 23:31:34 -0800

    fix primecheck.  a little faster way to find largest prime factor.  still slow for euler project question

--- euler3.sh
+++ euler3.sh
@@ -17,7 +17,7 @@ sqint=$(sqrt "$1")
 for i in $(eval echo "{2..$sqint}")
 do
   if [[ $1 -eq 2 || $1 -eq 3 ]]; then
-  echo 0
+  echo 1
   break 1
   elif [[ $1%$i -eq 0 ]]; then
   echo 0
@@ -29,27 +29,52 @@ done
 }
 
 
+#theres a better approach to this.  instead of making arrays, we just need to find the first prime going backwards through a loop.
+#I'll keep the old code commented below so I don't forget
 
-#lets make an array to get divisibles first
-array=()
-for x in $(eval echo "{1..$1}")
-do
-  if [[ $1%$x -eq 0 ]];
-    then
-    array+=("$x")
+for x in $(seq "$1" -1 1)
+ do
+   if [[ $1%$x -eq 0 && $(primecheck $x) -eq 1 ]];
+   then
+   printf '%s\n' "$x"
+   break 2
   fi
 done
 
 
+
+
+
+
+
+
+
+
+
+
+###OLD ARRAY CODE BELOW###
+#lets make an array to get divisibles first
+#array=()
+#for x in $(eval echo "{1..$1}")
+#do
+#  if [[ $1%$x -eq 0 ]];
+#    then
+#    array+=("$x")
+#  fi
+#done
+
+
 #now lets filter out the divisble array and keep only primes
-primearray=()
-for z in "${array[@]}";
-  do
-    if [[ $(primecheck "$z") -eq 1 ]];
-    then
-      primearray+=("$z")
-    fi
-  done
+#primearray=()
+#for z in "${array[@]}";
+#  do
+#    if [[ $(primecheck "$z") -eq 1 ]];
+#    then
+#      primearray+=("$z")
+#    fi
+#  done
 
 #largest prime factor should be last element in primearray
-printf '%s\n' "${primearray[-1]}"
+#printf '%s\n' "${primearray[-1]}"
+
+
